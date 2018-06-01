44f5e40a8b7956fab9dd5395ea40a6a2f9cb2f32
Arvin Maltezo Bareng <arvin.maltezo.bareng@gmail.com>
2018-06-02 01:20:24 +0800

    File Initialization

---
+++ November Project 01/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed; // Movement Speed of the Player
+	private Animator theAnimator; // Animator Used
+	private bool isMoving; // Is Player Moving
+	public Vector2 lastMove; // Last Movement in X and Y Axis
+
+	// Use this for initialization
+	void Start () {
+		theAnimator = GetComponent<Animator> (); // Reference to the Animator, will get the animator of the object this script is attached to
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		isMoving = false; // NOT moving
+
+		if (Input.GetAxisRaw ("Horizontal") > 0.5f || Input.GetAxisRaw ("Horizontal") < -0.5f) { // If you are pressing either A or D or Left or Right Arrow Keys, positive means to the right, negative value means to the left
+			transform.Translate (new Vector3 (Input.GetAxisRaw ("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f)); // Will make the player move in this axis at this amount
+			isMoving = true; // OBVIOUSLY moving
+			lastMove = new Vector2 (Input.GetAxisRaw("Horizontal"), 0f); // Getting the Last Movement
+		}
+
+		if (Input.GetAxisRaw ("Vertical") > 0.5f || Input.GetAxisRaw ("Vertical") < -0.5f) { // Same Logic as Above
+			transform.Translate (new Vector3 (0f, Input.GetAxisRaw ("Vertical") * moveSpeed * Time.deltaTime, 0f));
+			isMoving = true;
+			lastMove = new Vector2 (0f, Input.GetAxisRaw("Vertical"));
+		}
+
+		theAnimator.SetFloat ("moveX", Input.GetAxisRaw ("Horizontal")); // Setting the Value in Float Move X in Animator
+		theAnimator.SetFloat ("moveY", Input.GetAxisRaw ("Vertical")); // Same Logic as Above
+		theAnimator.SetBool ("isMoving", isMoving);
+		theAnimator.SetFloat ("lastMoveX", lastMove.x);
+		theAnimator.SetFloat ("lastMoveY", lastMove.y);
+	}
+}
---
+++ November Project 01/Assets/Tilesets/Tileset 01.tsx
@@ -0,0 +1,103 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tileset 01" tilewidth="16" tileheight="16" spacing="1" tilecount="1767" columns="57">
+ <image source="D:/Outlast/bin/Assets/GamesPlusJames/roguelikeSheet_transparent.png" width="968" height="526"/>
+ <terraintypes>
+  <terrain name="Dirt" tile="0"/>
+ </terraintypes>
+ <tile id="518" terrain="0,0,0,"/>
+ <tile id="519" terrain="0,0,,0"/>
+ <tile id="520" terrain=",,,0"/>
+ <tile id="521" terrain=",,0,0"/>
+ <tile id="522" terrain=",,0,"/>
+ <tile id="575" terrain="0,,0,0"/>
+ <tile id="576" terrain=",0,0,0"/>
+ <tile id="577" terrain=",0,,0"/>
+ <tile id="578" terrain="0,0,0,0"/>
+ <tile id="579" terrain="0,,0,"/>
+ <tile id="583">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0909091" y="0" width="15.8182" height="15.8182"/>
+  </objectgroup>
+ </tile>
+ <tile id="616">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.125" width="16.125" height="16.125"/>
+  </objectgroup>
+ </tile>
+ <tile id="617">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.125" width="15.875" height="16.375"/>
+  </objectgroup>
+ </tile>
+ <tile id="618">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.25" width="15.75" height="15.875"/>
+  </objectgroup>
+ </tile>
+ <tile id="619">
+  <objectgroup draworder="index">
+   <object id="1" x="0.375" y="-0.25" width="15.375" height="16.125"/>
+  </objectgroup>
+ </tile>
+ <tile id="634" terrain=",0,,"/>
+ <tile id="635" terrain="0,0,,"/>
+ <tile id="636" terrain="0,,,"/>
+ <tile id="640">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16.0909" height="16.0909"/>
+  </objectgroup>
+ </tile>
+ <tile id="673">
+  <objectgroup draworder="index">
+   <object id="1" x="1.5" y="-0.125" width="15" height="16.375"/>
+  </objectgroup>
+ </tile>
+ <tile id="674">
+  <objectgroup draworder="index">
+   <object id="1" x="0.125" y="-0.125" width="15.75" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="675">
+  <objectgroup draworder="index">
+   <object id="1" x="0.875" y="0.125" width="14.875" height="15.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="676">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.125" width="16.25" height="15.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="1358">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.0909091" y="0" width="16" height="15.8182"/>
+   <object id="2" x="-0.0909091" y="-0.0909091" width="16" height="16.2727"/>
+  </objectgroup>
+ </tile>
+ <tile id="1359">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.0909091" width="16.3636" height="16.0909"/>
+   <object id="2" x="0" y="0" width="16.3636" height="16.1818"/>
+  </objectgroup>
+ </tile>
+ <tile id="1360">
+  <objectgroup draworder="index">
+   <object id="1" x="0.181818" y="0.0909091" width="15.9091" height="16.0909"/>
+   <object id="2" x="0" y="0.181818" width="16.0909" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1361">
+  <objectgroup draworder="index">
+   <object id="6" x="12.8182" y="0" width="3" height="16.0909"/>
+  </objectgroup>
+ </tile>
+ <tile id="1362">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0909091" y="0.0909091" width="3.18182" height="16.1818"/>
+  </objectgroup>
+ </tile>
+ <tile id="1363">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="15.8182" height="15.8182"/>
+  </objectgroup>
+ </tile>
+</tileset>
